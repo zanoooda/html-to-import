5c85861749bf29e72f4a005d7ad6e49f3b932c0d
Igor Degtyarev <igord@cognility.com>
2023-02-09 13:05:51 +0200

    first commit

--- details.js
+++ details.js
@@ -1,49 +1,46 @@
+const titleQuestion = document.getElementById("titleQuestion");
+const answer = document.getElementById("answer");
+const titleAnswers = document.getElementById("titleAnswer");
 
-const titleQuestion = document.getElementById('titleQuestion')
-const answer = document.getElementById('answer')
-const titleAnswers = document.getElementById('titleAnswer')
+let url = new URL(location.href);
+let detailsTitle = JSON.parse(url.searchParams.get("data"));
+let indexObj = detailsTitle - 1;
 
-let url =new URL (location.href)
-let detailsTitle = JSON.parse(url.searchParams.get('data'))
-let indexObj = detailsTitle-1
-
-   const getDetailsTitle = ()=>{
-       let div = document.createElement('h1');
-      div.classList.add('titleQuestion')
-      div.textContent=`${array[0][indexObj].question}`
-       titleQuestion.appendChild(div)
-   }
-let array = []
+const getDetailsTitle = () => {
+    let div = document.createElement("h1");
+    div.classList.add("titleQuestion");
+    div.textContent = `${array[0][indexObj].question}`;
+    titleQuestion.appendChild(div);
+};
+let array = [];
 const getQuestion = async () => {
-   let arrayListQuestion=  await fetch('TV_FAQ_eng.json')
-       .then(res=>res.json())
-       .then(data=>data)
-   array.push(arrayListQuestion)
-}
-   const getAnswer = () => {
-   const detailsQuestion = array[0][indexObj]
-       const titleAnswer = detailsQuestion.answer.split('.')[0]
-       const indexDot=detailsQuestion.answer.indexOf('.')
-       let subAnswer =detailsQuestion.answer.slice(indexDot+1)
+    let arrayListQuestion = await fetch("TV_FAQ_eng.json")
+        .then((res) => res.json())
+        .then((data) => data);
+    array.push(arrayListQuestion);
+};
+const getAnswer = () => {
+    const detailsQuestion = array[0][indexObj];
+    const titleAnswer = detailsQuestion.answer.split(".")[0];
+    const indexDot = detailsQuestion.answer.indexOf(".");
+    let subAnswer = detailsQuestion.answer.slice(indexDot + 1);
 
-       let numberBlack = document.createElement('p');
-       let liTitle = document.createElement('h1');
-       let subAnswerP = document.createElement('li');
+    let numberBlack = document.createElement("p");
+    let liTitle = document.createElement("h1");
+    let subAnswerP = document.createElement("li");
 
-       numberBlack.classList.add('numberBlack')
-       numberBlack.textContent=` `
+    numberBlack.classList.add("numberBlack");
+    numberBlack.textContent = ` `;
 
-       liTitle.classList.add('listQuestionTitle')
-       liTitle.innerText=`${titleAnswer}.`
-       subAnswerP.classList.add('subAnswerP')
-       subAnswerP.innerText=`${subAnswer}`
-       titleAnswers.appendChild(numberBlack)
-       titleAnswers.appendChild(liTitle)
-          answer.appendChild(subAnswerP)
-
-   }
-getQuestion().then(()=> {
-    getDetailsTitle()
-    getAnswer()
-})
-
+    liTitle.classList.add("listQuestionTitle");
+    liTitle.innerText = `${titleAnswer}.`;
+    subAnswerP.classList.add("subAnswerP");
+    subAnswerP.innerText = `${subAnswer}`;
+    titleAnswers.appendChild(numberBlack);
+    titleAnswers.appendChild(liTitle);
+    answer.appendChild(subAnswerP);
+};
+getQuestion().then(() => {
+    getDetailsTitle();
+    getAnswer();
+});
--- listQuestions.js
+++ listQuestions.js
@@ -1,18 +1,19 @@
+const list = document.getElementById("list");
 
-const list = document.getElementById('list')
-
-let array = []
+let array = [];
 const getQuestion = async () => {
-    let arrayListQuestion=  await fetch('TV_FAQ_eng.json')
-        .then(res=>res.json())
-        .then(data=>data)
-array.push(arrayListQuestion)
-}
-getQuestion().then(()=>array[0].map((item,index)=>{
-    let div = document.createElement('a');
-    div.href = `./detailsQuestion.html?data=${index+1}`
-    div.classList.add('textQuestion')
-    div.textContent=`${index+1}) ${item.question}`
+    let arrayListQuestion = await fetch("TV_FAQ_eng.json")
+        .then((res) => res.json())
+        .then((data) => data);
+    array.push(arrayListQuestion);
+};
+getQuestion().then(() =>
+    array[0].map((item, index) => {
+        let div = document.createElement("a");
+        div.href = `./detailsQuestion.html?data=${index + 1}`;
+        div.classList.add("textQuestion");
+        div.textContent = `${index + 1}) ${item.question}`;
 
-    list.appendChild(div)
-}))
+        list.appendChild(div);
+    })
+);
